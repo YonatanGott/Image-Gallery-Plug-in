3d5446b11abf6dda11411799ecdc3d3d41bddd28
YonatanGott <godiner@gmail.com>
2021-02-16 09:00:36 +0200

    Update GalleryCarousel.js

--- src/components/GalleryCarousel.js
+++ src/components/GalleryCarousel.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import noImage from './noImage.png';
 import Carousel from 'react-bootstrap/Carousel';
-import GalleryImage from './GalleryImage'
 // Component Styling
 const useStyles = makeStyles({
     carouselTitle: {
@@ -12,7 +11,6 @@ const useStyles = makeStyles({
 
 const GalleryCarousel = (props) => {
     const classes = useStyles();
-    console.log(props);
     return (
         <Carousel
             className={classes.carousel}
@@ -21,12 +19,17 @@ const GalleryCarousel = (props) => {
             interval={props.interval}
         >
             {props.imageArray.map((image, index) => {
+
                 return (
                     <Carousel.Item key={index} image={image} >
                         <img
                             className="d-block w-100"
                             src={image.url}
-                            alt={noImage}
+                            alt="No Image"
+                            onError={(e) => {
+                                e.target.onerror = null;
+                                e.target.src = noImage;
+                            }}
                         />
                         <Carousel.Caption>
                             <h3 className={classes.carouselTitle}>{image.title}</h3>
